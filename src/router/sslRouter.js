3cc6a6e6e01d8e27478b1d816c3d404fde0c47ce
Emmanuel Kipmurkor <emmanuelkipmurkor@gmail.com>
2023-02-02 20:28:15 +0300

    end point to check status of ssl date

--- src/router/sslRouter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const sslCertController_1 = require("../Controllers/sslCertController");
-const sslRouter = (0, express_1.Router)();
-sslRouter.get('/ssl/expiration/date', sslCertController_1.expirationCertDate);
-exports.default = sslRouter;
